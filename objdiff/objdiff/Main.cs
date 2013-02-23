bd97c70d3dc1ea677dce7871dbfaa44ed23b0770
unknown <ciaran@KilgoreTrout.(none)>
2013-02-23 19:42:49 +0000

    Added stuff ?

--- objdiff/objdiff/Main.cs
+++ objdiff/objdiff/Main.cs
@@ -2,10 +2,34 @@ using System;
 
 namespace objdiff
 {
+	class Test1 
+	{
+		public int a { get; set; }
+		public bool b { get; set; }
+		public string c { get; set; }
+	}
+
 	class MainClass
 	{
 		public static void Main (string[] args)
 		{
+			var left = new Test1()
+			{
+				a = 1,
+				b = true,
+				c = "First string"
+			};
+			
+			var right = new Test1()
+			{
+				a = 2,
+				b = false,
+				c = "Second string"
+			};
+			
+			var comp = new ObjectDifference();
+			comp.ComparePorperties(left, right);
+
 			Console.WriteLine ("Hello World!");
 		}
 	}
--- objdiff/objdiff/Source/ObjectDifference.cs
+++ objdiff/objdiff/Source/ObjectDifference.cs
@@ -10,7 +10,18 @@ namespace objdiff
 		
 		public void ComparePorperties(object left, object right)
 		{
-			
+			var leftType = left.GetType();	
+			var rightType = right.GetType();
+
+			var leftProperties = leftType.GetProperties();
+			var rightProperties = rightType.GetProperties();
+
+
+			foreach (var leftProperty in leftProperties)
+			{
+				Console.WriteLine(leftProperty.Name + " " + leftProperty.GetValue(leftProperty, null).ToString());
+			}
+
 		}
 	}
 }
